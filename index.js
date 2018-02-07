042b986c7a1fa5fda67b44c00ea05e22abf630e1
LiYueLong <1393926171@qq.com>
2018-02-07 12:01:18 +0800

    设置禁止横屏

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Orientation from 'react-native-orientation';
+
+// 锁定竖屏
+Orientation.lockToPortrait();
 
 AppRegistry.registerComponent('guiren_rn', () => App);
