3b8afa5c7705fb2b58047461b868d3f027caf798
LiYueLong <1393926171@qq.com>
2018-02-07 17:40:39 +0800

    Revert "add amap"
    This reverts commit 2160176c101aa711941952079dbe037d28ddb79e.

--- src/module/start.js
+++ src/module/start.js
@@ -7,7 +7,7 @@
 import {NavigationActions} from 'react-navigation';
 import React, {Component} from 'react';
 import {getWidth, getHeight} from '../plugin/getSize';
-import Orientation from 'react-native-orientation';
+
 import {
     StyleSheet,
     View,
@@ -66,9 +66,6 @@ export default class Start extends Component {
             navigation.dispatch(resetAction);
 
         }, 1000);
-        
-        // 锁定竖屏
-        Orientation.lockToPortrait();
 
     }
 
